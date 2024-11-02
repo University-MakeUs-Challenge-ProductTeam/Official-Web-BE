f135784eea27040cbddb2abddf9f17eb991b67eb
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-11-02 14:32:34 +0900

    ♻️ Refactor: corsFilter 설정 수정

--- src/main/java/umc/product/web/global/config/CorsConfig.java
+++ src/main/java/umc/product/web/global/config/CorsConfig.java
@@ -1,15 +1,8 @@
 package umc.product.web.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -17,9 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
-                .allowedMethods("*") // 허용할 HTTP method
-                .allowCredentials(true); // 쿠키 인증 요청 허용
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(6000);
     }
 
 }
